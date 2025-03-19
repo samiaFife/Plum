b8cf4e249c04c57ed6d325d4f5e18e339a067576
Artur Khairullin <368983@niuitmo.ru>
2025-03-19 16:38:34 +0300

    changed model loader file as it was in example notebook

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,14 +1,17 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
+qconf = BitsAndBytesConfig(load_in_8bit=True)
 model_name = "AnatoliiPotapov/T-lite-instruct-0.1"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 if device == "cuda":
-    device_map = "auto"
+    device_map = "auto:0"
 else:
     device_map = None
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name, device_map=device_map)
+tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
+model = AutoModelForCausalLM.from_pretrained(
+    model_name, device_map=device_map, torch_dtype="auto", quantization_config=qconf
+)
 if device_map is None:
     model = model.to(device)
-model.eval()
+print(f"model {model_name.split('/')[1]} successully loaded on {model.device}")
